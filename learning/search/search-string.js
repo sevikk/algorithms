81323a50544c237fab8921ae3413a1c945538a4a
Sevikk <sevik.1989@gmail.com>
2023-07-12 18:41:19 -0500

    commit

--- learning/search/search-string.js
+++ learning/search/search-string.js
@@ -1,12 +1,45 @@
-function stringSearch(long, short){
-    var count = 0;
-    for(var i = 0; i < long.length; i++){
-        for(var j = 0; j < short.length; j++){
-           if(short[j] !== long[i+j]) break;
-           if(j === short.length - 1) count++;
-        }
+function stringSearch(long, short) {
+  var count = 0;
+  for (var i = 0; i < long.length; i++) {
+    for (var j = 0; j < short.length; j++) {
+      if (short[j] !== long[i + j]) break;
+      if (j === short.length - 1) count++;
     }
-    return count;
+  }
+  return count;
 }
 
-stringSearch("lorie loled", "lol")
+stringSearch("lorie loled", "lol");
+
+/** Function that count occurrences of a substring in a string;
+ * @param {String} string               The string
+ * @param {String} subString            The sub string to search for
+ * @param {Boolean} [allowOverlapping]  Optional. (Default:false)
+ */
+function occurrences(string, subString, allowOverlapping) {
+  string += "";
+  subString += "";
+  if (subString.length <= 0) return string.length + 1;
+
+  var n = 0,
+    pos = 0,
+    step = allowOverlapping ? 1 : subString.length;
+
+  while (true) {
+    pos = string.indexOf(subString, pos);
+    if (pos >= 0) {
+      ++n;
+      pos += step;
+    } else break;
+  }
+  return n;
+}
+
+occurrences("foofoofoo", "bar"); //0
+
+occurrences("foofoofoo", "foo"); //3
+
+occurrences("foofoofoo", "foofoo"); //1
+
+occurrences("foofoofoo", "foofoo", true); //2
+
